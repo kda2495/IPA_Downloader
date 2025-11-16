8ad538b510b04d6a8a7aa76ce9e746dd563ca8d7
Dmitry Ornatsky <140339869+kda2495@users.noreply.github.com>
2025-11-16 13:41:39 +0300

    Update IPA_Downloader_Eng.ps1
    Updated to 1.0.9.1 with small fixes

--- IPA_Downloader_Eng.ps1
+++ IPA_Downloader_Eng.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host "IPA_Downloader 1.0.9.1 (script by kda2495)" -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "IPA_Downloader 1.0.9.1 (script by kda2495)" -ForegroundColor Black -BackgroundColor Yellow
 if (!(Test-Path ".\Apps")) {
 	$null = New-Item -Path ".\Apps" -ItemType "Directory"
 }
@@ -169,8 +169,4 @@ while (Get-ChildItem -Path "$env:USERPROFILE\.ipatool" -Filter "account.") {
 			}
 		}
 	}
-
 }
-
-
-
